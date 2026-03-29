22026bbdcdaa6b7d714e8eda12b4a3546370091b
Mark Xue <mark@germ.network>
2026-03-29 09:34:23 -0700

    add any content type

--- Sources/GermConvenience/HTTPContentType.swift
+++ Sources/GermConvenience/HTTPContentType.swift
@@ -11,6 +11,8 @@ public struct HTTPContentType: Sendable {
 	public static let json = HTTPContentType(rawValue: "application/json")
 	public static let formData = HTTPContentType(
 		rawValue: "application/x-www-form-urlencoded;charset=UTF-8")
+	public static let any = HTTPContentType(
+		rawValue: "*/*")
 
 	public let rawValue: String
 }
