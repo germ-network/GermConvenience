7b55ac1370d69eda2d312cfb1aed0ca674242f35
Mark Xue <mark@germ.network>
2026-03-29 14:18:46 -0700

    equatable HTTPContentType

--- Sources/GermConvenience/HTTPContentType.swift
+++ Sources/GermConvenience/HTTPContentType.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct HTTPContentType: Sendable {
+public struct HTTPContentType: Equatable, Sendable {
 	public static let json = HTTPContentType(rawValue: "application/json")
 	public static let formData = HTTPContentType(
 		rawValue: "application/x-www-form-urlencoded;charset=UTF-8")
