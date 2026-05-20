7a1ccd47229e2c1f52f2f2a5c461157766c818d6
Mark Xue <mark@germ.network>
2026-05-20 02:13:16 -0700

    restore deleted urlscheme

---
+++ Sources/GermConvenience/HTTPTypes/URLScheme.swift
@@ -0,0 +1,14 @@
+//
+//  URLScheme.swift
+//  GermConvenience
+//
+//  Created by Mark @ Germ on 5/20/26.
+//
+
+
+public struct URLScheme: Sendable {
+	public static let http = URLScheme(rawValue: "http")
+	public static let https = URLScheme(rawValue: "https")
+
+	public let rawValue: String
+}
