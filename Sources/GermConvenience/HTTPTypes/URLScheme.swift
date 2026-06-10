953f5b74818908ef63f763a408fe83602e465e07
Mark Xue <mark@germ.network>
2026-06-10 12:02:36 -0700

    make the httpFetcher an actor to remove unchecked sendable

--- Sources/GermConvenience/HTTPTypes/URLScheme.swift
+++ Sources/GermConvenience/HTTPTypes/URLScheme.swift
@@ -5,7 +5,6 @@
 //  Created by Mark @ Germ on 5/20/26.
 //
 
-
 public struct URLScheme: Sendable {
 	public static let http = URLScheme(rawValue: "http")
 	public static let https = URLScheme(rawValue: "https")
